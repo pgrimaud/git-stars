6661180da99ad3bea22e347d1635480f96ade700
Julien Cousin-Alliot <Nisouc@outlook.com>
2021-05-27 11:43:51 +0200

    Add paginate helper

--- src/Controller/LanguageController.php
+++ src/Controller/LanguageController.php
@@ -9,6 +9,7 @@ use App\Repository\UserLanguageRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Utils\PaginateHelper;
 
 class LanguageController extends AbstractController
 {
@@ -36,7 +37,9 @@ class LanguageController extends AbstractController
         $userLanguages = $userLanguageRepository->findUserByLanguage($language, $start);
 
         $totalLanguagePages = $userLanguageRepository->totalLanguagePages($language);
-//        dd($tlp);
+
+        $paginate = PaginateHelper::create($page, $totalLanguagePages);
+        dd($totalLanguagePages, $paginate);
         return $this->render('language/show.html.twig', [
             'language'      => $language,
             'userLanguages' => $userLanguages,
--- src/Utils/PaginateHelper.php
+++ src/Utils/PaginateHelper.php
@@ -6,8 +6,52 @@ namespace App\Utils;
 
 class PaginateHelper
 {
-    public static function create(int $currentPage, int $maxResults, int $nbPerPage = 25)
+    public static function create(int $currentPage, int $maxResults, int $nbPerPage = 25): array
     {
+        $hasPrevious = true;
+        $hasNext     = true;
+
+        $totalPages = ceil($maxResults / $nbPerPage);
+
+
+        if ($currentPage == 1) {
+            $hasPrevious  = false;
+            $nextPage     = $currentPage + 1;
+            $nextPagePlus = $currentPage + 2;
+
+            return [
+                'currentPage'  => $currentPage,
+                'nextPage'     => $nextPage,
+                'nextPagePlus' => $nextPagePlus,
+                'resPerPage'   => $nbPerPage,
+                'totalPages'   => $totalPages,
+            ];
+
+        } else if ($currentPage == $totalPages) {
+            $hasNext          = false;
+            $previousPage     = $currentPage - 1;
+            $previousPagePlus = $currentPage - 2;
+
+            return [
+                'currentPage'      => $currentPage,
+                'previousPage'     => $previousPage,
+                'previousPagePlus' => $previousPagePlus,
+                'resPerPage'       => $nbPerPage,
+                'totalPages'       => $totalPages,
+            ];
+
+        } else {
+            $previousPage = $currentPage - 1;
+            $nextPage     = $currentPage + 1;
+
+            return [
+                'currentPage' => $currentPage,
+                'previous'    => $previousPage,
+                'nextPage'    => $nextPage,
+                'resPerPage'  => $nbPerPage,
+                'totalPages'  => $totalPages,
+            ];
+        }
 
     }
 }
