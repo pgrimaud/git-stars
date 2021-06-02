b97a49b9a180e6c34fe43fc4eee4e5e5195998ad
Julien Cousin-Alliot <Nisouc@outlook.com>
2021-06-02 12:00:28 +0200

    Added search user form

--- src/Controller/AppController.php
+++ src/Controller/AppController.php
@@ -4,25 +4,43 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
-use App\Message\UpdateUser;
-use App\Service\GeocodeService;
+use App\Entity\User;
+use App\Form\Model\Search;
+use App\Form\SearchType;
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class AppController extends AbstractController
 {
-    #[Route('/', name: 'app_index', methods: ['GET'])]
-    public function index(GeocodeService $service): Response
+    #[Route('/', name: 'app_index', methods: ['GET', 'POST'])]
+    public function index(Request $request, UserRepository $userRepository): Response
     {
-        $service->update(9999999999999, 'The internet');
+        $searchError = null;
 
-//        for ($i = 1; $i < 1000; ++$i) {
-//            $bus->dispatch(
-//                new UpdateUser($i)
-//            );
-//        }
+        $search = new Search();
 
-        return $this->render('app/index.html.twig');
+        $form = $this->createForm(SearchType::class, $search);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $search = $form->getData()->username;
+            $user   = $userRepository->findOneBy(['username' => $search]);
+
+            if (!$user instanceof User) {
+                $searchError = 'User ' . $search . ' was not found';
+            } else {
+                return $this->redirectToRoute('user_show', [
+                    'username' => $search,
+                ]);
+            }
+        }
+
+        return $this->render('app/index.html.twig', [
+            'search_form'  => $form->createView(),
+            'search_error' => $searchError,
+        ]);
     }
 }
---
+++ src/Form/SearchType.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Form;
+
+use App\Form\Model\Search;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class SearchType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('username', TextType::class)
+            ->add('submit', SubmitType::class)
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Search::class,
+        ]);
+    }
+}
