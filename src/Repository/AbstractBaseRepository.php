1c4534e77b840cbea90402ebe4216319a2b524d8
Pierre Grimaud <grimaud.pierre@gmail.com>
2021-06-04 16:37:43 +0200

    Update base reso di

--- src/Repository/AbstractBaseRepository.php
+++ src/Repository/AbstractBaseRepository.php
@@ -5,18 +5,19 @@ declare(strict_types=1);
 namespace App\Repository;
 
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Sentry\SentryBundle\Tracing\Cache\TraceableCacheAdapter;
 use Symfony\Component\Cache\Adapter\TraceableAdapter;
 
 abstract class AbstractBaseRepository extends ServiceEntityRepository
 {
-    private TraceableAdapter $cacheAdapter;
+    private TraceableCacheAdapter | TraceableAdapter $cacheAdapter;
 
-    public function setAdapter(TraceableAdapter $adapter): void
+    public function setAdapter(TraceableCacheAdapter | TraceableAdapter $adapter): void
     {
         $this->cacheAdapter = $adapter;
     }
 
-    public function getCacheAdapter(): TraceableAdapter
+    public function getCacheAdapter(): TraceableCacheAdapter | TraceableAdapter
     {
         return $this->cacheAdapter;
     }
