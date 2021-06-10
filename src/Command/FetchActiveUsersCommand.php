ac1173e1f7a0eab0f4ac156e0e7f9601df906431
Pierre Grimaud <grimaud.pierre@gmail.com>
2021-06-10 17:11:43 +0200

    Improve fetch active users command

--- src/Command/FetchActiveUsersCommand.php
+++ src/Command/FetchActiveUsersCommand.php
@@ -9,6 +9,7 @@ use App\Repository\UserRepository;
 use Google\Cloud\BigQuery\BigQueryClient;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
@@ -28,32 +29,47 @@ class FetchActiveUsersCommand extends Command
         parent::__construct($name);
     }
 
+    protected function configure(): void
+    {
+        $this->addArgument('limit', InputArgument::REQUIRED, 'Limit');
+    }
+
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
-        $io = new SymfonyStyle($input, $output);
-
+        $io   = new SymfonyStyle($input, $output);
         $date = new \DateTime('-2 days');
+
+        $limit = intval($input->getArgument('limit'));
+
+        if ($limit <= 0) {
+            $io->error('Limit must be superior to zero.');
+
+            return Command::FAILURE;
+        }
 
         $bigQuery = new BigQueryClient([
             'keyFile' => json_decode((string) file_get_contents(__DIR__ . '/../../gc-key.json'), true),
         ]);
 
-        $query = 'SELECT actor.id FROM `githubarchive.day.' . $date->format('Ymd') . '` GROUP BY actor.id';
+        $query = 'SELECT actor.id FROM `githubarchive.day.' . $date->format('Ymd') . '` GROUP BY actor.id LIMIT ' . $limit;
 
         $queryJobConfig = $bigQuery->query($query);
         $queryResults   = $bigQuery->runQuery($queryJobConfig);
 
         $users = $this->userRepository->findAllId();
 
+        $newUser = 0;
+
         foreach ($queryResults as $result) {
             if (!in_array($result['id'], $users)) {
                 $this->bus->dispatch(
                     new UpdateUser($result['id'])
                 );
+                ++$newUser;
             }
         }
 
-        $io->success('New users added!');
+        $io->success($newUser . ' new user(s) added!');
 
         return Command::SUCCESS;
     }
