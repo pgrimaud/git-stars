10a4c1e9607017d390c2e3bb2ba0258ff9579622
Pierre Grimaud <grimaud.pierre@gmail.com>
2021-06-14 12:16:14 +0200

    Fix migration

--- migrations/Version20210614101142.php
+++ migrations/Version20210614101142.php
@@ -28,4 +28,9 @@ final class Version20210614101142 extends AbstractMigration
         // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('ALTER TABLE country DROP iso_code');
     }
+
+    public function isTransactional(): bool
+    {
+        return false;
+    }
 }
