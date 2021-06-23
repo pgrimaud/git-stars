f751d7681a869738f032c3b0223c49446f49d271
Pierre Grimaud <grimaud.pierre@gmail.com>
2021-06-23 17:46:47 +0200

    Update CI/CD

--- assets/app.js
+++ assets/app.js
@@ -2,5 +2,5 @@ import './vendor/bootstrap/css/bootstrap.min.css';
 import './vendor/autocomplete/css/auto-complete.css';
 import './styles/style.css';
 
-import './vendor/bootstrap/js/bootstrap.bundle.min';
-import './vendor/autocomplete/js/auto-complete.min';
+import './vendor/bootstrap/js/bootstrap.bundle.min.js';
+import './vendor/autocomplete/js/auto-complete.min.js';
