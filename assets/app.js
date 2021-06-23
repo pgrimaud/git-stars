d9c7e0526379e67bbcb8414974d2a64233519c95
Pierre Grimaud <grimaud.pierre@gmail.com>
2021-06-23 18:21:10 +0200

    Fix auto complete language

--- assets/app.js
+++ assets/app.js
@@ -2,5 +2,6 @@ import './vendor/bootstrap/css/bootstrap.min.css';
 import './vendor/autocomplete/css/auto-complete.css';
 import './styles/style.css';
 
-import './vendor/bootstrap/js/bootstrap.bundle.min.js';
-import './vendor/autocomplete/js/auto-complete.min.js';
+import './vendor/bootstrap/js/bootstrap.bundle.min'
+
+global.autoComplete = require ('./vendor/autocomplete/js/auto-complete.min.js');
